b12ab5078ccf370b4d8a94ca3cc5c7685a772b9d
o0V0o <o0V0o@users.noreply.github.com>
2017-01-24 16:22:21 -0600

    testing triangle wave gear

--- init.lua
+++ init.lua
@@ -16,8 +16,15 @@ local step = false
 
 --local gearset = GearSet(Functions.Constant(1), Functions.Constant(1)+2, 6)
 local offset = 1.5
+
+-- sin wave gear
 local gearset = GearSet(Functions.Constant(1), Functions.normalize(Functions.Sine()+offset, math.pi*2, 2*math.pi), 6, 25, 30, anim)
+
+--circular gear.
 --local gearset = GearSet(Functions.Constant(1), Functions.Constant(1), 6, 4)
+
+-- triangle wave gear
+local gearset = GearSet(  Functions.Constant(1), Functions.normalize(Functions.LinearSpline( { vec2(0, slow), vec2(math.pi, fast), vec2(math.pi*2, slow)}), math.pi*2, math.pi*2), 6, 25, 30, anim)
 
 if anim then
 	require'cutanim'(gearset, true)
